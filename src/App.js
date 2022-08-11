80dd9dbc3160a130364d28d8cc881f9a25f7b09f
quangoc98 <quanglm.classfunc@gmail.com>
2022-08-11 12:13:49 +0700

    design header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Header from "./pages/Layouts/Header";
 import {Route, Routes} from "react-router-dom";
 import Footer from "./pages/Layouts/Footer";
-import HomePage from "./pages/Layouts/Home";
-import CoffeePage from "./pages/Layouts/Coffee";
+import HomePage from "./pages/Home";
+import CoffeePage from "./pages/Coffee";
+import TeaPage from "./pages/Tea";
+import DrinkPage from "./pages/Drinks";
+import ProductPage from "./pages/Products";
+import SalePage from "./pages/Sales";
+import AboutPage from "./pages/About";
+import CardPage from "./pages/Card";
 
 function App() {
   return (
@@ -10,11 +16,13 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<HomePage/>}/>
-          <Route path='/' element={<CoffeePage/>}/>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/' element={<HomePage/>}/>
+          <Route path='/coffee' element={<CoffeePage/>}/>
+          <Route path='/tea' element={<TeaPage/>}/>
+          <Route path='/drinks' element={<DrinkPage/>}/>
+          <Route path='/products' element={<ProductPage/>}/>
+          <Route path='/sales' element={<SalePage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/card' element={<CardPage/>}/>
         </Routes>
         <Footer/>
       </div>
--- src/pages/Layouts/Header/index.jsx
+++ src/pages/Layouts/Header/index.jsx
@@ -1,5 +1,24 @@
+import ButtonComponent from "../../../components/ButtonComponent"
+import delivery from "./images/delivery.png";
+import logo from "./images/logo_2.png"
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 export default function Header() {
     return (
-        <div></div>
+        <div className="w-full h-[100px] flex px-[100px] py-[25px] justify-between">
+            <img src={delivery} alt='delivery'/>
+            <img src={logo} alt='logo'/>
+            <div className="flex items-center gap-4">
+                <ButtonComponent className='font-bold' name='Đăng nhập'/>
+                <div className="flex items-center">
+                    <ButtonComponent className='border-r-2 border-green-700 pr-2 text-green-700' name='VN'/>
+                    <ButtonComponent className='pl-2' name='EN'/>
+                </div>
+                <ButtonComponent 
+                    className='px-2 py-1 gap-2 border-2 rounded-md border-green-700 text-green-700' 
+                    name='giỏ hàng' 
+                    icon={<ShoppingCartOutlinedIcon/>}/>
+            </div>
+        </div>
     )
 }

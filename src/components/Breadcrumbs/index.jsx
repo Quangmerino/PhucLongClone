fa04311664cdc4521a33de1811680bda293830c6
quangoc98 <quanglm.classfunc@gmail.com>
2022-08-27 03:26:12 +0700

    fix style breadcrumbs and add images banner cf

--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -5,11 +5,11 @@ export default function Breadcrumbs(props) {
   const {breadcrumbs} = props;
   return (
       <ul class="flex justify-center items-center my-4 text-[18px] w-[800px] gap-2">
-        <li className="text-gray-400">
+        <li className="text-gray-400 hover:text-red-500">
           <Link to='/'>Home</Link>
         </li>
         <RightOutlined style={{fontSize:'12px'}}/>
-        <li className="text-gray-600">
+        <li className="text-gray-600 hover:text-red-500">
           <Link to=''>
             {breadcrumbs}
           </Link>

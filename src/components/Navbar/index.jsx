c13a4cd7280f1e370348848d10ed735b43912f6c
quangoc98 <quanglm.classfunc@gmail.com>
2022-08-30 10:20:50 +0700

    update code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,11 @@
 import { Link } from "react-router-dom";
-import {navigations} from "../../data";
-import { Button, Popover } from "antd";
+import { navigations } from "../../data";
 import { SearchOutlined } from "@ant-design/icons";
+import ButtonCustom from "../ButtonComponent";
 
 // import 'antd/dist/antd.css';
 
 export default function Navigation() {
-  const PopoverSearch = (
-    <ul className="z-50 flex flex-col gap-4 p-4 bg-white rounded-sm shadow-2xl w-[200px]">
-      <li>
-        <Link to="/">Sản phẩm trà</Link>
-      </li>
-      <li>
-        <Link to="/">Sản phẩm cà phê</Link>
-      </li>
-    </ul>
-  );
   return (
     <div className="flex items-center gap-8 justify-center border-y-2 border-gray-200">
       <ul className="flex py-2 justify-center">
@@ -34,15 +24,18 @@ export default function Navigation() {
         </li>
       </ul>
       <div>
-        <Popover
-          placement="bottomRight"
-          content={PopoverSearch}
-          trigger="hover"
-        >
-          <Button className="flex items-center">
-            <SearchOutlined style={{ fontSize: "20px" }} />
-          </Button>
-        </Popover>
+        <ButtonCustom
+          className="flex items-center"
+          icon={<SearchOutlined style={{ fontSize: "20px" }} />}
+        />
+        {/* <ul className="z-10 flex flex-col gap-4 p-4 bg-white rounded-sm shadow-2xl w-[200px]">
+          <li>
+            <Link to="/">Sản phẩm trà</Link>
+          </li>
+          <li>
+            <Link to="/">Sản phẩm cà phê</Link>
+          </li>
+        </ul> */}
       </div>
     </div>
   );

df02240a827e12f44d5603bbf369f0fa8a8f4e08
quangoc98 <quanglm.classfunc@gmail.com>
2022-09-01 01:02:56 +0700

    #26 responsive drink page

--- src/pages/Drinks/index.jsx
+++ src/pages/Drinks/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import ButtonCustom from "../../components/ButtonComponent";
 import { CardProduct } from "../../components/Card";
 import HeaderContent from "../../components/HeaderContent";
@@ -15,18 +16,18 @@ export default function DrinkPage() {
         title="Thức uống"
       />
       <div className="">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-8 p-20">
+        <div className="grid grid-cols-1 p-6 gap-y-8 md:grid-cols-2 md:gap-10 md:p-10 lg:grid-cols-3 lg:p-20 lg:px-10 xl:grid-cols-4">
           {dataDrinks.map((item, index) => {
             return (
               <CardProduct
                 key={index}
                 image={item.img}
-                className="flex flex-col items-center gap-1.5 relative bg-white hover:shadow-lg hover:border rounded-md py-7 px-12"
+                className="flex flex-col items-center border gap-1.5 rounded-md shadow-lg relative py-7 bg-white md:hover:shadow-lg md:hover:border md:px-12 lg:border-0 lg:pt-2 lg:shadow-none xl:pt-7"
                 classNameChild="flex flex-col items-center gap-2"
               >
-                <span className="text-black font-semibold truncate overflow-hidden max-w-[200px]">
+                <a href="#" title={item.name} className="text-black font-semibold truncate overflow-hidden max-w-[200px]">
                   {item.name}
-                </span>
+                </a>
                 {item.sub_name !== "" ? (
                   <span className="truncate overflow-hidden max-w-[200px]">
                     {item.sub_name}
@@ -35,7 +36,7 @@ export default function DrinkPage() {
                   <span className="text-white">phuc long</span>
                 )}
                 <span className="text-green-700 font-bold">{item.price} đ</span>
-                <ButtonCustom className="" name="ĐẶT HÀNG" />
+                <ButtonCustom className="text-green-700 rounded-md py-1 px-4 border border-green-700 hover:bg-green-700 hover:text-white" name="ĐẶT HÀNG" />
               </CardProduct>
             );
           })}

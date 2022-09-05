2162399e42805cfb67206568a528cdf1599e30ab
quangoc98 <quanglm.classfunc@gmail.com>
2022-09-05 14:29:08 +0700

    add homepage github

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,22 +1,17 @@
 import { useState } from "react";
 
-export default function TabsCustom({tabs, children}) {
+export default function TabsCustom({ tabs, children }) {
   const [tabName, setTabName] = useState(0);
-  const [type, setType] = useState() ;
-    return (
-        <div>
-            {
-                tabs.map((tab, index)=>{
-                    return(
-                        <button 
-                            key={index}
-                            onClick={()=> setType(tab)}
-                        >
-                            {tab}
-                        </button>
-                    )
-                })
-            }
-        </div>
-      )
+  const [type, setType] = useState();
+  return (
+    <div>
+      {tabs.map((tab, index) => {
+        return (
+          <button key={index} onClick={() => setType(tab)}>
+            {tab}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
